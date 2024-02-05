8fd0a0362f137bab75672ff8ef0581438b5646e8
Mostafa <kahanimostafa83@gmail.com>
2024-02-05 03:33:01 +0330

    add project with librelys

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
 import "@/styles/globals.css";
+import createEmotionCache from "./createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import RTL from "./RTL";
+import Head from "next/head";
+import { CssBaseline } from "@mui/material";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <RTL>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <meta name="robots" content="index" />
+          <meta name="googlebot" content="index" />
+          <title>Vixxa | ویکسا</title>
+        </Head>
+        <div>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </div>
+      </RTL>
+    </CacheProvider>
+  );
 }
