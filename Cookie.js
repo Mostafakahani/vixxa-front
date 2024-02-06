73cb2c908352fd819b8accb0a7d905c569ee1d79
Mostafa <kahanimostafa83@gmail.com>
2024-02-07 03:00:17 +0330

    setCookie

--- Cookie.js
+++ Cookie.js
@@ -5,10 +5,18 @@ export function setCookie(name, value, days) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie =
-    name + "=" + (value || "") + expires + "; path=/; domain=." + name;
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
-
+// export function setCookie(name, value, days) {
+//   var expires = "";
+//   if (days) {
+//     var date = new Date();
+//     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+//     expires = "; expires=" + date.toUTCString();
+//   }
+//   document.cookie =
+//     name + "=" + (value || "") + expires + "; path=/; domain=." + name;
+// }
 export function getCookie(name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(";");
