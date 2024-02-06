5a4b00b6fea8ed0dacc96944a218aa717cbe98ae
Mostafa <kahanimostafa83@gmail.com>
2024-02-06 21:38:58 +0330

    fix footer

--- Componenets/Layout/Footer.jsx
+++ Componenets/Layout/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <Container maxWidth={false} disableGutters>
+        <footer>
+          <Grid
+            container
+            sx={{
+              boxShadow: "0px 9px 120px -15px #00000075",
+              px: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" sx={{ fontWeight: 400, fontSize: 12 }}>
+             ویکسا | طراحی و توسعه Mostafa
+            </Typography>
+          </Grid>
+        </footer>
+      </Container>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
