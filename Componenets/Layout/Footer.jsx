5778ecbebfdaaeebaf7dceb5d90b2d56c99dd41e
Mostafa <kahanimostafa83@gmail.com>
2024-03-02 20:40:33 +0330

    add ZP

--- Componenets/Layout/Footer.jsx
+++ Componenets/Layout/Footer.jsx
@@ -1,8 +1,10 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 function Footer() {
+  const router = useRouter();
   return (
     <footer
       style={{
@@ -15,21 +17,48 @@ function Footer() {
       }}
     >
       <Container disableGutters>
-        <Grid container justifyContent="center" alignItems="center">
-          <a
-            referrerPolicy="origin"
-            target="_blank"
-            href="https://trustseal.enamad.ir/?id=459381&Code=a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
+        <Grid container justifyContent="center" alignItems="center" spacing={2}>
+          <Grid
+            container
+            item
+            justifyContent="center"
+            alignItems="center"
+            xs={2}
           >
-            <img
-              referrerPolicy="origin"
-              src="https://trustseal.enamad.ir/logo.aspx?id=459381&Code=a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
-              alt=""
-              style={{ cursor: "pointer" }}
-              code="a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
+            <Box
+              component={"img"}
+              src="/zp.svg"
+              width={55}
+              height={"auto"}
+              sx={{ cursor: "pointer" }}
+              onClick={() =>
+                router.push("https://www.zarinpal.com/trustPage/vixxa.ir")
+              }
             />
-          </a>
+          </Grid>
+          <Grid
+            container
+            item
+            justifyContent="center"
+            alignItems="center"
+            xs={2}
+          >
+            <a
+              referrerPolicy="origin"
+              target="_blank"
+              href="https://trustseal.enamad.ir/?id=459381&Code=a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
+            >
+              <img
+                referrerPolicy="origin"
+                src="https://trustseal.enamad.ir/logo.aspx?id=459381&Code=a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
+                alt=""
+                style={{ cursor: "pointer" }}
+                code="a0WmTJGMv4OXj2kc7EhFe35pjxySWEuh"
+              />
+            </a>
+          </Grid>
         </Grid>
+
         <Grid container justifyContent="center" alignItems="center">
           <Typography
             variant="body2"
