4954c2877aef3aee333c3fe766402e5f57e7a6db
Mostafa <kahanimostafa83@gmail.com>
2024-03-17 22:08:48 +0330

    add liara config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const ServerDeveloperMode = true;
+export const ServerDeveloperMode = false;
 export const Server = {
   URL: ServerDeveloperMode
     ? "http://127.0.0.1:3000"
@@ -6,5 +6,5 @@ export const Server = {
   SECRET:
     process.env.SECRET ||
     "a417644571a7a6fa842002f2836bce80a2ea7ed15df2ea76403098b381b16e5b",
-  developerMode: true,
+  developerMode: false,
 };
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   // pageExtensions: ["page.js"],
+  output: "standalone",
   env: {
     // BASE_URL: process.env.BASE_URL,
     // SECRET: process.env.SECRET,
